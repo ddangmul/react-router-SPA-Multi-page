56094e8a9ac49b16d6749a03e7f21572828bc9d0
ddangmul <j.ddangmul@gmail.com>
2024-12-31 20:55:14 +0900

    라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 
-// 라우트 정의
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+// 라우터 정의
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
 function App() {
+  // 라우터 사용
   return <RouterProvider router={router} />;
 }
 
