10a2ce0e2d782b2153de20dd08c037add6b475d0
ddangmul <j.ddangmul@gmail.com>
2025-01-02 17:59:22 +0900

    errorElement 라우터 속성 추가해 오류페이지 표시

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />, // pages 컴포넌트들의 래퍼 역할
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
