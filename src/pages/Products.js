b970f4d8763ff44b1e5f2e5c230ee5cf2d1d2f5a
ddangmul <j.ddangmul@gmail.com>
2025-01-02 19:01:55 +0900

    동적 라우트에 링크 추가

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,3 +1,23 @@
+import { Link } from "react-router-dom";
+// 클릭 시 링크가 하이라이트퇴지 않고, 현재 페이지를 떠날 거라서 NavLink가 아닌 Link 사용
+
+const PRODUCTS = [
+  { id: "p1", title: "Product 1" },
+  { id: "p2", title: "Product 2" },
+  { id: "p3", title: "Product 3" },
+];
+
 export default function ProductsPage() {
-  return <h1>The Products Page</h1>;
+  return (
+    <>
+      <h1>The Products Page</h1>
+      <ul>
+        {PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
