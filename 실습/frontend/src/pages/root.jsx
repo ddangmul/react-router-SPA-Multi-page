47b94ebbeb8787b256e6aeefbba7f4c94bcbe752
ddangmul <j.ddangmul@gmail.com>
2025-01-03 23:18:43 +0900

    useNavigation()으로  현재 네비게이션 상태 UI에  반영

--- 실습/frontend/src/pages/root.jsx
+++ 실습/frontend/src/pages/root.jsx
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router-dom";
-
+import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 export default function RootLayout() {
+  const navigation = useNavigation(); // navigation 객체 반환
+
   return (
     <>
       <MainNavigation />
       <main>
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
